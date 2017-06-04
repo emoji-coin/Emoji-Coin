49d2bd3446382c76b01d4661a88f71137ef32d09
Findblockscom <mark@findblocks.com>
2017-06-04 15:38:50 +0100

    Few updates

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define Emoji-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "EmojiURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
